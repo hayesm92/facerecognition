11c7e943f2b467f4feda20a53ee45b82b222d7bf
hayesm92 <46799216+hayesm92@users.noreply.github.com>
2020-01-26 21:00:51 -0500

    Updates

---
+++ precache-manifest.c3500e098229db4cca4ec1e92dbb0055.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e46f55250f18aefa3b14950235ab47",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "ed553e007993079b9254",
+    "url": "/facerecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f703fb9a540e68007e1a",
+    "url": "/facerecognition/static/css/main.88782d27.chunk.css"
+  },
+  {
+    "revision": "ed553e007993079b9254",
+    "url": "/facerecognition/static/js/2.14013927.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/facerecognition/static/js/2.14013927.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f703fb9a540e68007e1a",
+    "url": "/facerecognition/static/js/main.d535c06f.chunk.js"
+  },
+  {
+    "revision": "4c43526d50500c64c0a4",
+    "url": "/facerecognition/static/js/runtime-main.3b48aae8.js"
+  }
+]);
