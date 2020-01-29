94140a0dac64af6ae4ecbfec21516b57c6fa15b8
hayesm92 <46799216+hayesm92@users.noreply.github.com>
2020-01-28 21:58:49 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ loadUser = (data) =>{
 
   onButtonSubmit = () =>{
     this.setState({imageUrl: this.state.input})
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://fast-sea-27249.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -109,7 +109,7 @@ loadUser = (data) =>{
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://fast-sea-27249.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
